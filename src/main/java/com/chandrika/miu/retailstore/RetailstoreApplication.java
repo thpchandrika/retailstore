47e4ade6629d907af2b71abd585eee8f682b3fac
thpchandrika <thpchandrika@gmail.com>
2024-03-01 20:48:24 -0600

    file changed for testing webhooks

--- src/main/java/com/chandrika/miu/retailstore/RetailstoreApplication.java
+++ src/main/java/com/chandrika/miu/retailstore/RetailstoreApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RetailstoreApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Running springboot application");
 		SpringApplication.run(RetailstoreApplication.class, args);
 	}
 

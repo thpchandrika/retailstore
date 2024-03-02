9761aa76651b96745af6fed326c5de3924a13d6f
Chandrika Thapa <119996274+thpchandrika@users.noreply.github.com>
2024-03-01 20:51:18 -0600

    Update RetailstoreApplication.java

--- src/main/java/com/chandrika/miu/retailstore/RetailstoreApplication.java
+++ src/main/java/com/chandrika/miu/retailstore/RetailstoreApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RetailstoreApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Running springboot application");
+	public static void main(String[] args) {		
 		SpringApplication.run(RetailstoreApplication.class, args);
 	}
 
